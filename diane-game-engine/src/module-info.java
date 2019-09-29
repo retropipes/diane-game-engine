cc68761a62ebd8e6f1311fd5960c9c469e39f501
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-09-29 11:07:37 -0400

    Make Diane usable as a dependency.

--- diane-game-engine/src/module-info.java
+++ diane-game-engine/src/module-info.java
@@ -1,3 +1,19 @@
 module com.puttysoftware.diane {
     requires transitive java.desktop;
+    exports com.puttysoftware.commondialogs;
+    exports com.puttysoftware.diane.loaders;
+    exports com.puttysoftware.errorlogger;
+    exports com.puttysoftware.fileutils;
+    exports com.puttysoftware.help;
+    exports com.puttysoftware.images;
+    exports com.puttysoftware.images.edges;
+    exports com.puttysoftware.integration;
+    exports com.puttysoftware.page;
+    exports com.puttysoftware.password;
+    exports com.puttysoftware.picturepicker;
+    exports com.puttysoftware.randomrange;
+    exports com.puttysoftware.scoremanager;
+    exports com.puttysoftware.storage;
+    exports com.puttysoftware.updater;
+    exports com.puttysoftware.xio;
 }
