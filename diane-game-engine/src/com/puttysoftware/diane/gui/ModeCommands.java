5bf55e9b4a9c0b51ba452682850272391dbaee7e
RetroPipes <eric.ahnell@puttysoftware.com>
2022-11-02 12:08:18 -0400

    Add ModeCommands interface to Diane.

---
+++ diane-game-engine/src/com/puttysoftware/diane/gui/ModeCommands.java
@@ -0,0 +1,23 @@
+package com.puttysoftware.diane.gui;
+
+public interface ModeCommands {
+    void enterMode();
+    
+    void exitMode();
+    
+    void enableModeCommands();
+
+    void disableModeCommands();
+
+    void setInitialState();
+
+    void createCommandsPane();
+
+    void enableLoadedCommands();
+
+    void disableLoadedCommands();
+
+    void enableDirtyCommands();
+
+    void disableDirtyCommands();
+}
